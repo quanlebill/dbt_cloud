98329e7ba7b70b7abbbf849c3f8d1aa099a0742c
lemin <leminhquan.19.01.04@gmail.com>
2026-03-23 17:35:03 -0400

    adding staging.yml

--- models/staging/snowflake/stg_snowflake__imf_nominal_gdp.sql
+++ models/staging/snowflake/stg_snowflake__imf_nominal_gdp.sql
@@ -1,19 +1,41 @@
-with 
+{{ config(materialized='view') }}
+with
 
 source as (
     select * from {{source('snowflake', 'imf_nominal_gdp')}}
 ),
 
+raw as (
+    select
+        country as country_name,
+        LOWER(type),
+        cast(year as int) as year,
+        population_change_pct as population_change,
+        gdp_nominal_usd as gdp_nominal,
+        gdp_per_capita_usd as gdp_per_capita,
+        gdp_growth_pct as gdp_growth
+    from source
+    where country is not null
+    and type is not null
+    and LOWER(type) in ["history", "projection"]
+    and year is not null
+    and year > 0
+    and gdp_nominal_usd > 0
+    and gdp_per_capita_usd > 0
+)
+
+--making sure no duplicate in combination (country_name, type, year)
 final as (
     select
-        country,
-        type ,
+        country_name,
+        type,
         year,
-        population_change_pct,
-        gdp_nominal_usd,
-        gdp_per_capita_usd,
-        gdp_growth_pct
-    from source
+        AVG(population_change) as population_change,
+        AVG(gdp_nominal) as gdp_nominal,
+        AVG(gdp_per_capita) as gdp_per_capita,
+        AVG(gdp_growth) as gdp_growth
+    from raw
+    group by country_name, type, year
 )
 
 select * from final
--- models/staging/snowflake/stg_snowflake__imf_ppp_gdp.sql
+++ models/staging/snowflake/stg_snowflake__imf_ppp_gdp.sql
@@ -1,19 +1,42 @@
-with 
+{{ config(materialized='view') }}
+with
 
 source as (
     select * from {{source('snowflake', 'imf_ppp_gdp')}}
 ),
 
+raw as (
+    select
+        country as country_name,
+        LOWER(type),
+        cast(year as int) as year,
+        population_change_pct as population_change,
+        gdp_ppp_usd as gdp_ppp,
+        gdp_ppp_per_capita_usd as gdp_ppp_per_capita,
+        gdp_ppp_growth_pct as gdp_ppp_growth
+    from source
+    where country is not null
+    and type is not null
+    and LOWER(type) in ["history", "projection"]
+    and year is not null
+    and year > 0
+    and gdp_ppp_usd > 0
+    and gdp_ppp_per_capita_usd > 0
+)
+
+--making sure no duplicate in combination (country_name, type, year)
 final as (
     select
-        country,
+        country_name,
         type,
         year,
-        population_change_pct,
-        gdp_ppp_usd,
-        gdp_ppp_per_capita_usd,
-        gdp_ppp_growth_pct
-    from source
+        AVG(population_change) as population_change,
+        AVG(gdp_ppp) as gdp_ppp,
+        AVG(gdp_ppp_per_capita) as gdp_ppp_per_capita,
+        AVG(gdp_ppp_growth) as gdp_ppp_per_growth
+    from raw
+    group by country_name, type, name
 )
+
 
 select * from final
